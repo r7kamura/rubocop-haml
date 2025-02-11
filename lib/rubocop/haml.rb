0e8e3b83ce21c1494daa576e7edabd21e2d37d9b
Koichi ITO <koic.ito@gmail.com>
2025-02-12 02:48:37 +0900

    Pluginfy rubocop-haml
    This PR makes rubocop-haml support RuboCop's Plugin feature.
    https://github.com/rubocop/rubocop/pull/13792
    
    It replaces the ad-hoc `Inject` with RuboCop plugins introduced in RuboCop 1.72.
    Additionally, since RuboCop already requires Ruby 2.7 or higher as its runtime,
    the `required_ruby_version` will be updated to 2.7.

--- lib/rubocop/haml.rb
+++ lib/rubocop/haml.rb
@@ -2,12 +2,11 @@
 
 module RuboCop
   module Haml
-    autoload :ConfigLoader, 'rubocop/haml/config_loader'
     autoload :RubyClip, 'rubocop/haml/ruby_clip'
     autoload :RubyClipper, 'rubocop/haml/ruby_clipper'
     autoload :RubyExtractor, 'rubocop/haml/ruby_extractor'
   end
 end
 
-require_relative 'haml/rubocop_extension'
+require_relative 'haml/plugin'
 require_relative 'haml/version'
